d4c52a6bb52149f473908e81ccbcfdb524c70846
Facundo Fernández <ffernanndez97@gmail.com>
2024-10-22 23:57:56 -0300

    Update sea_level_predictor.py
    Created scatter plot and added labels

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -4,20 +4,20 @@ from scipy.stats import linregress
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-
+    plt.scatter(df.Year, df.CSIRO Adjusted Sea Level)
 
     # Create first line of best fit
-
+    
 
     # Create second line of best fit
 
 
     # Add labels and title
-
-    
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
