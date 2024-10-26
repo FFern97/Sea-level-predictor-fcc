4bf930cf58d9f36ede4f54a7c330627f3e4546b1
Facundo Fernández <ffernanndez97@gmail.com>
2024-10-25 23:47:12 -0300

    Update sea_level_predictor.py
    Line of best fit on y-axis

--- sea_level_predictor.py
+++ sea_level_predictor.py
@@ -7,17 +7,23 @@ def draw_plot():
     df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-    plt.scatter(df.Year, df.CSIRO Adjusted Sea Level)
+    plt.scatter(df.['Year'], df.['CSIRO Adjusted Sea Level'])
 
     # Create first line of best fit
-    
+    slope, intercept, r, p, std_err = stats.linregress(df.['Year'], df.['CSIRO Adjusted Sea Level'])
 
+    x_value = range(df.['Year'].min(), 2061)
+    y_value = slope * x_value + intercept
+    plt.plot(x_value, y_value)
+
+    2050_prediction = slope * 2050 + intercept 
     # Create second line of best fit
 
 
     # Add labels and title
     plt.xlabel('Year')
     plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
     # Save plot and return data for testing (DO NOT MODIFY)
     plt.savefig('sea_level_plot.png')
     return plt.gca()
